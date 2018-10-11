cafc439bed1cd22fa5056b152f2308858bd81df6
Heidi Engstrom <hengstrom25@gmail.com>
2018-10-11 16:25:17 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
           if session[:name] == nil || session[:name].empty?
             redirect_to sessions_new_path
           else
-        redirect_to '/'
+            redirect_to '/'
     end
     
     def destroy
